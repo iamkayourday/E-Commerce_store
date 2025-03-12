7be85d2f1759f482e9c482858d5017a90c1f41eb
Abdulbasit <abdulbasitkayode@gmail.com>
2025-03-12 12:11:14 +0100

    add routing and splash screen functionality with animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import BottomHeader from './Components/BottomHeader';
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+import Favorites from './Pages/Favorites';
+import Messages from './Pages/Messages';
+import Profile from './Pages/Profile';
+import Splash from './Components/Splash';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    // Hide the splash screen after 3 seconds
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 1000); // 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      {showSplash ? (
+        <Splash />
+      ) : (
+        <>
+          <div className="pb-16"> {/* Add padding-bottom to avoid content overlap with the bottom header */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/messages" element={<Messages />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </div>
+          <BottomHeader />
+        </>
+      )}
+    </Router>
+  );
 }
 
-export default App
+export default App;
